3e7a7411131ba29f558df545eaedff8c33c92a30
Stanislav <61699457+pauker-vielleicht@users.noreply.github.com>
2022-11-25 01:52:43 +0300

    Add files via upload

---
+++ first_task/3.2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace example
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int size = Convert.ToInt32(Console.ReadLine()); 
+            string s = Console.ReadLine();
+            string[] ar = s.Split(' ');
+
+            for ( int i = 0; i < size; i++)
+            {
+                for ( int j = 0; j < size - 1; j++)
+                {
+                    if (Convert.ToInt32(ar[j]) > Convert.ToInt32(ar[j + 1]))
+                    {
+                        string temp = ar[j + 1];
+                        ar[j + 1] = ar[j];
+                        ar[j] = temp;
+                    }
+                }
+            }
+            int k = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(ar[k - 1]);
+        }
+    }
+}
