760f06d142aa4e4b4f2cf2b2ff395ac35cd13121
Stanislav <61699457+pauker-vielleicht@users.noreply.github.com>
2022-11-25 01:52:05 +0300

    Add files via upload

---
+++ first_task/3.1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace example
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] data = s.Split(' ');
+            string array = Console.ReadLine();
+            string[] ar = array.Split(' ');
+
+
+            int size = Convert.ToInt32(data[0]);
+
+            double p = Convert.ToInt32(data[1]),
+                   sum = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                double temp = Convert.ToInt32(ar[i]);
+                sum += Math.Pow(temp, p);
+            }
+
+            Console.WriteLine(Math.Exp(Math.Log(sum)/p));
+
+        }
+    }
+}
