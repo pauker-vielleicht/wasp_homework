baa0582201f679f1325776e6cb4b2700e683bd3e
Stanislav <61699457+pauker-vielleicht@users.noreply.github.com>
2022-11-25 01:48:37 +0300

    Add files via upload

---
+++ first_task/1.2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace example
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int size = Convert.ToInt32(Console.ReadLine()),
+                left = size,
+                right = 1;
+
+            int[,] ar = new int[size,size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int l = left; l < size; l++)
+                {
+                    Console.Write(l.ToString() + ' ');
+                }
+                for (int r = size; r >= right; r--)
+                {
+                    Console.Write(r.ToString() + ' ');
+                }
+                left--;
+                right++;
+                Console.WriteLine();
+            }
+        }
+    }
+}
