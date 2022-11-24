6b79a4634bf34bb95130467a5d071f828b099bf2
Stanislav <61699457+pauker-vielleicht@users.noreply.github.com>
2022-11-25 01:51:08 +0300

    Add files via upload

---
+++ first_task/2.1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace example
+{
+    class Program
+    { 
+        public static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            string s = "";
+            while ( num != 0 )
+            {
+                s = (num & 1).ToString() + s;
+                num >>= 1;
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
